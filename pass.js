35a8b5660199cf6e6955066c0badc5288f819c6f
neha111993 <sonineha1330@gmail.com>
2026-01-26 13:39:10 +0530

    expense

--- pass.js
+++ pass.js
@@ -1,16 +1,21 @@
-let eyeicon = document.getElementById("eyeicon");
-let password = document.getElementById("password");
+const password = document.getElementById("password");
 
-eyeicon.onclick =function()
-{
-    if (password.type = "password")
-    {
-        password.type="text";
-        eyeicon.src='OIPO.jpg';
+function genPassword(){
+    var chars = "0123456789abcdefghijklmnopqrstuv!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var passLength = 10;
+    var password = "";
+
+    for(var i = 0; i < passLength; i++){
+        var randomNumber = Math.floor(Math.random()* chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1)
     }
-    else
-    {
-        password.type ="password";
-        eyeicon.src ='OIPC.jpg';
-    }
+
+    document.getElementById("password").value = password;
+}
+
+function copyPassword(){
+    var copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");
 }
